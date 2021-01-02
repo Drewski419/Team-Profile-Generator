c330ead35737c862b1fcbf33ed83226044f4461f
Bryan Escudero <Ajefferson419@yahoo.com>
2021-01-02 02:28:55 -0800

    more updates

--- app.js
+++ app.js
@@ -79,14 +79,6 @@ function addMember() {
     });
 }
 
-// function renderHtml(memberArray) {
-//     startHtml();
-//     for (const member of memberArray) {
-//         addHtml(member);
-//     }
-//     finishHtml();
-// }
-
 function startHtml() {
     const html = `<!DOCTYPE html>
     <html lang="en">
@@ -181,10 +173,4 @@ function finishHtml() {
     console.log("end");
 }
 
-// addMember();
-// startHtml();
-// addHtml("hi")
-// .then(function() {
-// finishHtml();
-// });
 initApp();
